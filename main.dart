710c6bc97887b69b48695a12e9ea3dc0875603bf
Cairyl Ella Agullo <168385988+AgulloCairyl14@users.noreply.github.com>
2025-08-08 17:37:46 +0800

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,10 @@
+void main() {
+    String greeting = "Welcome";
+    String user = "Alice";
+
+    print("$greeting, $user!");
+
+    dynamic dynamicUser = 25;
+
+    print("$greeting, $user!");
+}
